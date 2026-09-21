cdc26309434085e79b5f5ac5ea8f819efd24c831
Nicolas De Lima Dos Santos <nc.limasnts@gmail.com>
2026-09-21 20:08:17 -0300

    feat: adiciona arquivo userAccount HTML,CSS,TS SPEC.TS

---
+++ fluxo/src/app/features/user-account/user-account.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserAccount } from './user-account';
+
+describe('UserAccount', () => {
+  let component: UserAccount;
+  let fixture: ComponentFixture<UserAccount>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [UserAccount],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(UserAccount);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
